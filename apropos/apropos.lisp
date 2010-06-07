cd59a554ea94458ce1507c8682bb2053a9dd3b5b
Kenneth Tilton <kentilton@gmail.com>
2010-06-07 04:04:48 -0400

    port param to serve-apropos

--- apropos/apropos.lisp
+++ apropos/apropos.lisp
@@ -1,35 +1,34 @@
 (in-package :qooxlisp)
 
 
-(defun serve-apropos ()
-  (let ((port 8000))
+(defun serve-apropos (&optional (port 8000))
+  
+  (when *wserver* (shutdown))
+  (qx-reset)
+  (net.aserve:start :debug t :port port)
+  (flet ((pfl (p f)
+           (publish-file :port port
+             :path p
+             :file f))
+         (pdr (p d)
+           (publish-directory :port port
+             :prefix p
+             :destination d))
+         (pfn (p fn)
+           (publish :path p :function fn)))
     
-    (when *wserver* (shutdown))
-    (qx-reset)
-    (net.aserve:start :debug t :port port)
-    (flet ((pfl (p f)
-             (publish-file :port port
-               :path p
-               :file f))
-           (pdr (p d)
-             (publish-directory :port port
-               :prefix p
-               :destination d))
-           (pfn (p fn)
-             (publish :path p :function fn)))
-      
-      (pdr "/qx/" "/devel/qx/")
-      (pfn "/begin" 'qx-begin) ;; <=== qx-begin (below) gets customized
-      (pfn "/callback" 'qx-callback-js)
-      (pfn "/cbjson" 'qx-callback-json)
-      
-      (let* ((app-root "/devel/qooxlisp/ide") ;; <=== change this to point to your qooxdoo app
-             (app-source (format nil "~a/source/" app-root)))
-        (flet ((src-ext (x)
-                 (format nil "~a~a" app-source x)))
-          (pfl "/" (src-ext "index.html"))
-          (pdr "/source/" app-source)
-          (pdr "/script/" (src-ext "script/")))))))
+    (pdr "/qx/" "/devel/qx/")
+    (pfn "/begin" 'qx-begin) ;; <=== qx-begin (below) gets customized
+    (pfn "/callback" 'qx-callback-js)
+    (pfn "/cbjson" 'qx-callback-json)
+    
+    (let* ((app-root "/devel/qooxlisp/ide") ;; <=== change this to point to your qooxdoo app
+           (app-source (format nil "~a/source/" app-root)))
+      (flet ((src-ext (x)
+               (format nil "~a~a" app-source x)))
+        (pfl "/" (src-ext "index.html"))
+        (pdr "/source/" app-source)
+        (pdr "/script/" (src-ext "script/"))))))
 
 (defun qx-begin (req ent)
   (ukt::stop-check :qx-begin)
